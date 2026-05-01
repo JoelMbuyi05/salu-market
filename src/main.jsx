d4b4be0bf7455ca751a179c590c02ff2792cf243
Joel Mbuyi <165423496+JoelMbuyi05@users.noreply.github.com>
2026-05-01 23:54:14 +0200

    Initial setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,20 @@
+import { AuthProvider } from './store/authContext'
+import ReactDOM from 'react-dom';
 import { StrictMode } from 'react'
 import './lib/i18n'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
