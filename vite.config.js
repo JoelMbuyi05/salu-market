6807861739f1176fc3156686930d2198c8f60e33
Joel Mbuyi <165423496+JoelMbuyi05@users.noreply.github.com>
2026-05-28 00:26:16 +0200

    fix: single icon reference to fix double logo on Android

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       injectRegister: 'inline',
-      includeAssets: ['favicon.png', 'icon-192.png', 'icon-512.png'],
+      includeAssets: ['favicon.png', 'icon-512.png'],
       manifest: {
         name: 'Salu Market',
         short_name: 'Salu',
@@ -21,18 +21,13 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: 'icon-192.png',
-            sizes: '192x192',
+            src: 'icon-512.png',
+            sizes: '1254x1254',
             type: 'image/png'
           },
           {
             src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
+            sizes: '1254x1254',
             type: 'image/png',
             purpose: 'maskable'
           }
