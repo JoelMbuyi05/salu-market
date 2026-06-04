94d9630b744fd438ad1001969f71363bb0e96c03
Joel Mbuyi <165423496+JoelMbuyi05@users.noreply.github.com>
2026-06-04 13:28:25 +0200

    fix: manualchuncks for vercel build

--- vite.config.js
+++ vite.config.js
@@ -5,30 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (!id.includes('node_modules')) return;
-
-          if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
-            return 'vendor';
-          }
-
-          if (id.includes('@supabase')) {
-            return 'supabase';
-          }
-
-          if (
-            id.includes('i18next') ||
-            id.includes('react-i18next') ||
-            id.includes('i18next-browser-languagedetector')
-          ) {
-            return 'i18n';
-          }
-        }
-              }
-            }
-          },
+  },
   plugins: [
     react(),
     VitePWA({
